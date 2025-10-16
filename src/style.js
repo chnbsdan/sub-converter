12ef01381e4cc4d97c13e62f45a563ff409d166d
chnbsdan <chnbsdan@gmail.com>
2025-10-16 20:13:07 +0800

    Update style.js

--- src/style.js
+++ src/style.js
@@ -70,7 +70,7 @@ export function generateStyles() {
     left: 0;
     width: 100%;
     height: 100%;
-    background: rgba(0, 0, 0, 0.4);
+    background: rgba(0, 0, 0, 0.1);
     z-index: -1;
   }
 
